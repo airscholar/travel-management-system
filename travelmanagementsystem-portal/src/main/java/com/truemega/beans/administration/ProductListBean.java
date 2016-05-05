b86bafb1f4512ed5eb0f93b19a9a62990c724058
IbrahimAmmar <ibrahim.ammar@truemega.com>
2016-05-05 14:51:08 +0300

    product portal

---
+++ travelmanagementsystem-portal/src/main/java/com/truemega/beans/administration/ProductListBean.java
@@ -0,0 +1,116 @@
+package com.truemega.beans.administration;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.event.AjaxBehaviorEvent;
+
+import com.truemega.beans.TravelListBean;
+import com.truemega.dto.ProductTypeDTO;
+import com.truemega.dto.ServiceTypeDTO;
+import com.truemega.interfaces.administration.ProductService;
+import com.truemega.interfaces.administration.ServiceService;
+
+@ManagedBean(name = "productlist")
+@ViewScoped
+public class ProductListBean extends TravelListBean {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@EJB
+	ServiceService serviceService;
+	@EJB
+	ProductService productService;
+
+	List<ServiceTypeDTO> serviceTypeDTOs;
+	List<ProductTypeDTO> productTypeDTOs;
+
+	@Override
+	public String getScreenName() {
+		// TODO Auto-generated method stub
+		return "administration/productlist.xhtml";
+	}
+
+	@Override
+	public void search() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@PostConstruct
+	@Override
+	public void load() {
+		// TODO Auto-generated method stub
+		try {
+			serviceTypeDTOs = serviceService.getAllServices(getUserName());
+			System.out.println("serviceTypeDTOs =====->"
+					+ serviceTypeDTOs.size());
+
+			productTypeDTOs = productService.getAllProduct(getUserName());
+			System.out.println("productTypeDTOs =====->"
+					+ productTypeDTOs.size());
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+
+	}
+
+	public void changeStatus(AjaxBehaviorEvent event) {
+
+		try {
+			ProductTypeDTO product = (ProductTypeDTO) event.getComponent()
+					.getAttributes().get("productmodel");
+
+			productService.changeStatus(product.getStatus(), product.getId(),
+					getUserName());
+
+			load();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public boolean validate() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	public List<ServiceTypeDTO> getServiceTypeDTOs() {
+		return serviceTypeDTOs;
+	}
+
+	public void setServiceTypeDTOs(List<ServiceTypeDTO> serviceTypeDTOs) {
+		this.serviceTypeDTOs = serviceTypeDTOs;
+	}
+
+	public List<ProductTypeDTO> getProductTypeDTOs() {
+		return productTypeDTOs;
+	}
+
+	public void setProductTypeDTOs(List<ProductTypeDTO> productTypeDTOs) {
+		this.productTypeDTOs = productTypeDTOs;
+	}
+
+}
---
+++ travelmanagementsystem-portal/src/main/java/com/truemega/converter/ServiceConverter.java
@@ -0,0 +1,67 @@
+package com.truemega.converter;
+
+import java.util.Iterator;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+
+import com.truemega.dto.ServiceTypeDTO;
+import com.truemega.logger.LoggerService;
+
+public class ServiceConverter  implements Converter{
+
+	private List<ServiceTypeDTO> services;
+	private LoggerService loggerService = new LoggerService();
+	public ServiceConverter(List<ServiceTypeDTO> services) {
+		this.services = services;
+	}
+
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component,
+			String value) {
+		// TODO Auto-generated method stub
+		loggerService.logPortalInfo(" start getAsObject method of ServiceConverter ");
+		if (value != null && value.trim().length() > 0
+				&& (!value.equals("None")&& value.trim().length() > 0)) {
+			try {
+				ServiceTypeDTO selectedService = new ServiceTypeDTO();
+				selectedService.setId(new Integer(value));
+				for (Iterator iterator = services.iterator(); iterator
+						.hasNext();) {
+					ServiceTypeDTO serviceDTO = (ServiceTypeDTO) iterator.next();
+					if (serviceDTO.equals(selectedService)){
+						loggerService.logPortalInfo(" end getAsObject method of ServiceConverter ");
+						return serviceDTO;
+					}
+						
+				}
+			} catch (NumberFormatException e) {
+				loggerService.logPortalError("Not a valid Selected Accident", e);
+				throw new ConverterException(new FacesMessage(
+						FacesMessage.SEVERITY_ERROR, "Conversion Error",
+						"Not a valid theme."));
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component,
+			Object object) {
+		// TODO Auto-generated method stub
+		loggerService.logPortalInfo(" start getAsString  method of ServiceConverter ");
+		if (object instanceof String)
+			return null;
+		if (object != null) {
+			return String.valueOf(((ServiceTypeDTO) object).getId());
+		} else {
+			return null;
+		}
+
+	}
+
+}
