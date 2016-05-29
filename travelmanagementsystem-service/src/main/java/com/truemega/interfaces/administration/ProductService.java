4e3c2d9a7f7f09ac21e4fc5e63cf19644b5792cf
Youssef <youssef.abdelnabi@truemega.com>
2016-05-29 12:03:31 +0200

    Prodcut Service

--- travelmanagementsystem-service/src/main/java/com/truemega/interfaces/administration/ProductService.java
+++ travelmanagementsystem-service/src/main/java/com/truemega/interfaces/administration/ProductService.java
@@ -5,21 +5,29 @@ import java.util.List;
 import javax.ejb.Remote;
 
 import com.truemega.dto.ProductTypeDTO;
+import com.truemega.dto.ServiceTypeDTO;
 
 @Remote
-public interface ProductService  {
-	
-	public ProductTypeDTO saveProduct(ProductTypeDTO productTypeDTO , String userName);
+public interface ProductService {
 
-	public ProductTypeDTO updateProduct(ProductTypeDTO productTypeDTO , String userName);
+	public ProductTypeDTO saveProduct(ProductTypeDTO productTypeDTO,
+			String userName);
 
-	public ProductTypeDTO findProductById(Integer productId , String userName);
+	public ProductTypeDTO updateProduct(ProductTypeDTO productTypeDTO,
+			String userName);
+
+	public ProductTypeDTO findProductById(Integer productId, String userName);
 
 	public List<ProductTypeDTO> getAllProduct(String userName);
 
-    public void changeStatus(Boolean status, Integer productId,
- 			String userName );
-    public boolean checkUniqueProductWithService(String productName ,Integer serviceId);
-    public List<ProductTypeDTO> getAllProductActive(String userName);
+	public void changeStatus(Boolean status, Integer productId, String userName);
+
+	public boolean checkUniqueProductWithService(String productName,
+			Integer serviceId);
+
+	public List<ProductTypeDTO> getAllProductActive(String userName);
+
+	public List<ProductTypeDTO> getProductsByService(
+			ServiceTypeDTO serviceTypeDTO);
 
 }
